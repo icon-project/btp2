a1fc80af3380e7177c430a4d172b09c428801df2
Jaechang Namgoong <jnamgoong@parametacorp.com>
2023-05-17 15:44:55 +0900

    Use keysecret file instead of using raw password string

--- e2edemo/relay.sh
+++ e2edemo/relay.sh
@@ -20,13 +20,23 @@ DST_BMC_ADDRESS=$(cat ${DEPLOYMENTS} | jq -r .${DST}.contracts.bmc)
 SRC_ADDRESS=btp://${SRC_NETWORK}/${SRC_BMC_ADDRESS}
 SRC_ENDPOINT=$(cat ${CHAIN_CONFIG} | jq -r .chains.${SRC}.endpoint)
 SRC_KEY_STORE=$(cat ${CHAIN_CONFIG} | jq -r .chains.${SRC}.keystore)
-SRC_KEY_PASSWORD=$(cat ${CHAIN_CONFIG} | jq -r .chains.${SRC}.keypass)
+SRC_KEY_SECRET=$(cat ${CHAIN_CONFIG} | jq -r .chains.${SRC}.keysecret)
+if [ ${SRC_KEY_SECRET} != null ]; then
+  SRC_KEY_PASSWORD=$(cat ${SRC_KEY_SECRET})
+else
+  SRC_KEY_PASSWORD=$(cat ${CHAIN_CONFIG} | jq -r .chains.${SRC}.keypass)
+fi
 
 # DST network config
 DST_ADDRESS=btp://${DST_NETWORK}/${DST_BMC_ADDRESS}
 DST_ENDPOINT=$(cat ${CHAIN_CONFIG} | jq -r .chains.${DST}.endpoint)
 DST_KEY_STORE=$(cat ${CHAIN_CONFIG} | jq -r .chains.${DST}.keystore)
-DST_KEY_PASSWORD=$(cat ${CHAIN_CONFIG} | jq -r .chains.${DST}.keypass)
+DST_KEY_SECRET=$(cat ${CHAIN_CONFIG} | jq -r .chains.${DST}.keysecret)
+if [ ${DST_KEY_SECRET} != null ]; then
+  DST_KEY_PASSWORD=$(cat ${DST_KEY_SECRET})
+else
+  DST_KEY_PASSWORD=$(cat ${CHAIN_CONFIG} | jq -r .chains.${DST}.keypass)
+fi
 
 if [ "x$BMV_BRIDGE" = xtrue ]; then
   echo "Using Bridge mode"
