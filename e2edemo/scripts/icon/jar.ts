c3d60b26cc3b0c4b44b0b55008f13ca71c899c19
Jaechang Namgoong <jnamgoong@parametacorp.com>
2023-12-21 16:41:02 +0900

    Find the optimized jar with pattern

--- e2edemo/scripts/icon/jar.ts
+++ e2edemo/scripts/icon/jar.ts
@@ -2,14 +2,21 @@ import fs from 'fs';
 import path from 'path';
 
 export class Jar {
-  public static readFromFile(base: string | undefined, project: string, version: string = '0.1.0') {
+  public static readFromFile(base: string | undefined, project: string, version?: string) {
     if (!base) {
       base = "../javascore";
     }
     const build = "build/libs";
+    const basedir = path.join(base, project, build);
     const name = project.replace("/", "-");
-    const optJar = `${name}-${version}-optimized.jar`;
-    const fullPath = path.join(base, project, build, optJar);
+    let optJar;
+    if (version) {
+      optJar = `${name}-${version}-optimized.jar`;
+    } else {
+      const files = fs.readdirSync(basedir);
+      optJar = files.filter(f => f.match(`${name}\\-[\\d.]+\\-optimized.jar`))[0];
+    }
+    const fullPath = path.join(basedir, optJar);
     return fs.readFileSync(fullPath).toString('hex')
   }
 }
